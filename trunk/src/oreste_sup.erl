dc6872b2002d7815ff12170f5f278d456834b9e5
matteo.redaelli <matteo.redaelli@f207ac04-b03d-11de-8db9-a33e7b47b29e>
2011-08-28 19:06:42 +0000

    added license in all files

--- trunk/src/oreste_sup.erl
+++ trunk/src/oreste_sup.erl
@@ -1,7 +1,22 @@
-%% @author author <author@example.com>
-%% @copyright YYYY author.
-
+%% Copyright (C) 2010,2011 ~ matteo DOT redaelli AT libero DOT it
+%% http://www.redaelli.org/matteo/
+%%
+%% This program is free software: you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published by
+%% the Free Software Foundation, either version 3 of the License, or
+%% (at your option) any later version.
+%%
+%% This program is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+%% GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public License
+%% along with this program. If not, see <http://www.gnu.org/licenses/>.
+%%
 %% @doc Supervisor for the oreste application.
+%% 
+%% project url: http://code.google.com/p/oreste/
 
 -module(oreste_sup).
 -author('author <author@example.com>').
