2f01aeaeec25542feae4b53eacf6670f60daf412
matteo.redaelli <matteo.redaelli@f207ac04-b03d-11de-8db9-a33e7b47b29e>
2011-08-30 17:57:45 +0000

    added env sys.config file and timeout parameter for sql_query

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd `dirname $0`
-exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -s odbc -s oreste
+exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -s odbc -s oreste -config sys
